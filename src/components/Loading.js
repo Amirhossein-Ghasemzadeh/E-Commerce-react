eb2e1476074a025b2a3e53410b96a3836a6854e1
Amirhossein_Ghasemzadeh <amirghasemzadeh17@gmail.com>
2021-10-29 20:08:40 +0330

    fix spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Loading = () => {
-  return <div className="section section-center">loading </div>;
+  return <div className="section section-center">
+    <div className="loading"></div>
+  </div>;
 };
 
 export default Loading;
